b1e06668778631e06c7c823dee463ec0d973a06b
Revanth <154988667+revanthbethala@users.noreply.github.com>
2024-11-23 13:42:32 +0530

    second commit

--- src/FetchedData.jsx
+++ src/FetchedData.jsx
@@ -1,42 +1,69 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react"
+import { useState } from "react";
 
 export default function FetchedData({ response }) {
-   const [fetchedData, setFetchedData] = useState([])
-   setFetchedData(response?.results.map((result, id) => {
-      return (
-         {
-            id: id,
-            question: result.question,
-            options: [...result.incorrect_answers, result.correct_answer],
-            correctAnswer: result.correct_answer
-         }
-      )
-   }))
+  const [count, setCount] = useState(0);
+  const data = response?.results.map((result, id) => {
+    return {
+      id: id,
+      question: result.question,
+      options: [...result.incorrect_answers, result.correct_answer].sort(),
+      correctAnswer: result.correct_answer,
+    };
+  });
 
-   return (
-      <div>
-         <Questions results={fetchedData} />
-      </div>
-   )
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center gap-6">
+      <Questions
+        results={data[count]}
+        setCount={setCount}
+        count={count}
+        size={data.length}
+      />
+    </div>
+  );
 }
-function Questions({ results }) {
-   const [count, setCount] = useState(0)
-   return (
-      <div>
-         <p>{results.question}</p>
-         <div>
-            <ul>
-               {results.options.sort().map(
-                  (option, index) => (
-                     <li key={index}>{results.options} </li>
-                  ))}
-            </ul>
-            <div>
-               {count < results.length && <button onClick={() => setCount(count + 1)}>{"Next"}</button>}
-               {count > 0 && <button onClick={() => setCount(count - 1)}>{"Back"}</button>}
-            </div>
-         </div>
+function Questions({ results, setCount, size, count }) {
+  const [option, setOption] = useState(null);
+  const [isActive, setIsActive] = useState(false);
+  console.log(isActive);
+
+  return (
+    <div>
+      <p>{results.question}</p>
+      <div className="flex flex-col">
+        {results.options.map((option, index) => (
+          <button
+            key={index}
+            className={`rounded-xl m-2 p-3 font-semibold text-zinc-800 `}
+            value={option}
+            onClick={(e) => {
+              setOption(e.target.value);
+              setIsActive(true);
+            }}
+            disabled={isActive}
+          >
+            {option}
+          </button>
+        ))}
       </div>
-   )
+      <div>
+        {count < size - 1 && (
+          <button
+            onClick={() => {
+              setCount(count + 1);
+              setIsActive(false);
+              setOption("");
+            }}
+            className="bg-violet-500 p-3 rounded-lg text-white"
+          >
+            Next
+          </button>
+        )}
+        {option === results.correctAnswer && isActive
+          ? "Correct Answer"
+          : "Incorrect Answer"}
+      </div>
+    </div>
+  );
 }
